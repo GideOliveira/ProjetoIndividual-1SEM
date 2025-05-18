71b04b4c44a4f6b3b7b85d104cb4f510af56bee2
GideOliveira <giovanna.doliveira@sptech.school>
2025-05-18 19:28:17 -0300

    quiz nível fácil feito

---
+++ public/js/cadastro.js
@@ -0,0 +1,95 @@
+var listaUsuariosCadastrados = [];
+
+function cadastrar() {
+    var nome = input_nome.value;
+    var email = input_email.value;
+    var senha = input_senha.value;
+    var confirmarSenha = input_confirmar_senha.value;
+
+    if (nome == "" || email == "" || senha == "" || confirmarSenha == "") {
+        cardErro.style.display = "block";
+        mensagem_erro.innerHTML =
+        "(Mensagem de erro para todos os campos em branco)";
+
+        finalizarAguardar();
+        return false;
+    } else {
+      setInterval(sumirMensagem, 5000);
+    }
+
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // crie um atributo que recebe o valor recuperado aqui
+        // Agora vá para o arquivo routes/usuario.js
+        nomeServer: nomeVar,
+        emailServer: emailVar,
+        senhaServer: senhaVar,
+        idEmpresaVincularServer: idEmpresaVincular,
+        cpfserver: cpfvar
+      }),
+    })
+      .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+          cardErro.style.display = "block";
+
+          mensagem_erro.innerHTML =
+            "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
+          setTimeout(() => {
+            window.location = "login.html";
+          }, "2000");
+
+          limparFormulario();
+          finalizarAguardar();
+        } else {
+          throw "Houve um erro ao tentar realizar o cadastro!";
+        }
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+        finalizarAguardar();
+      });
+
+    return false;
+  }
+
+  // Listando empresas cadastradas 
+  function listar() {
+    fetch("/empresas/listar", {
+      method: "GET",
+    })
+      .then(function (resposta) {
+        resposta.json().then((empresas) => {
+          empresas.forEach((empresa) => {
+            listaEmpresasCadastradas.push(empresa);
+
+            console.log("listaEmpresasCadastradas")
+            console.log(listaEmpresasCadastradas[0].codigo_ativacao)
+          });
+        });
+      })
+      .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+      });
+  }
+
+    function sumirMensagem() {
+        cardErro.style.display = "none";
+    }
---
+++ public/js/quiz.js
@@ -0,0 +1,131 @@
+const questions = [
+    {
+        question: "O que significa MPIF?",
+        answers: [
+            { id: 1, text: "Meninas Perigosas e Incrivelmente Focadas", correct: false },
+            { id: 2, text: "Mulher Preta Independente de Favela", correct: true },
+            { id: 3, text: "Meninas Poderosas Independente de Favela", correct: false },
+            { id: 4, text: "Mulheres Periféricas Inspirando Futuro", correct: false },
+        ]
+    },
+    {
+        question: "De onde Tasha e Tracie são?",
+        answers: [
+            { id: 1, text: "Curitiba, PR", correct: false },
+            { id: 2, text: "Rio de Janeiro, RJ", correct: false },
+            { id: 3, text: "Fortaleza, CE", correct: false },
+            { id: 4, text: "São Paulo, SP", correct: true },
+        ]
+    },
+    {
+        question: "Qual o parentesco de Tasha e Tracie?",
+        answers: [
+            { id: 1, text: "Primas", correct: false },
+            { id: 2, text: "Amigas", correct: false },
+            { id: 3, text: "Irmãs Gêmeas", correct: true },
+            { id: 4, text: "Mãe e filha", correct: false },
+        ]
+    },
+    {
+        question: "Qual é a frase das gêmeas?",
+        answers: [
+            { id: 1, text: "Fé em Deus que ele é justo", correct: false },
+            { id: 2, text: "Tudo dá certo pra mim, até quando não foi como eu quis", correct: true },
+            { id: 3, text: "Eu só confio em mim, mais ninguém, cê me entende!", correct: false },
+            { id: 4, text: "Humanidade é má, e até Jesus Chorou", correct: false },
+        ]
+    },
+    {
+        question: "O que é ROUFFERS?",
+        answers: [
+            { id: 1, text: "Os fãs das gêmeas", correct: true },
+            { id: 2, text: "Os amigos das gêmeas", correct: false },
+            { id: 3, text: "A mãe das gêmeas", correct: false },
+            { id: 4, text: "O pai das gêmeas", correct: false },
+        ]
+    },
+]
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+var currentQuestionIndex = 0;
+var score = 0;
+
+function ComecarQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Próxima";
+    mostrarQuestao();
+}
+
+function redefinirEstado() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function mostrarQuestao() {
+    redefinirEstado();
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.dataset.id = answer.id;
+        button.classList.add("btn");
+        button.addEventListener("click", selectAnswer);
+        answerButtons.appendChild(button);
+    })
+}
+
+function selectAnswer(e) {
+    answers = questions[currentQuestionIndex].answers;
+    const correctAnswer = answers.filter((answer) => answer.correct == true)[0];
+
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.id == correctAnswer.id;
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach((button) => {
+        if (button.dataset.id == correctAnswer.id) {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    redefinirEstado();
+    questionElement.innerHTML = `Você acertou ${score} de ${questions.length}`;
+    nextButton.innerHTML = "Jogue novamente";
+    nextButton.style.display = "block"; 
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        mostrarQuestao();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        ComecarQuiz();
+    }
+})
+
+ComecarQuiz();
